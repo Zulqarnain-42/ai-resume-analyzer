a76c41eb91d6b44cd14c27baf27243dc17b4576f
Zulqarnain <zulqarnain.maqsood42@gmail.comm>
2025-08-12 12:39:15 +0400

    making the home page

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,30 @@
+import Navbar from "~/components/Navbar";
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
+import { resumes } from "constants";
+import ResumeCard from "~/components/ResumeCard";
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "Ai Resume Analyzer" },
+    { name: "description", content: "Smart Resume Analyzer!" },
   ];
 }
 
 export default function Home() {
-  return <Welcome />;
+  return <main className="bg-[url('/images/bg-main.svg')] bg-cover">
+    <Navbar />
+    <section className="main-section">
+      <div className="page-heading py-16">
+        <h1>Track Your Applications & Resume Ratings</h1>
+        <h2>Review Your Submission and check AI-powered feedback.</h2>
+      </div>
+      {resumes && resumes.length > 0 && (
+        <div className="resumes-section">
+          {resumes.map((resume:Resume) => (
+            <ResumeCard key={resume.id} resume={resume} />
+          ))}
+        </div>
+      )}
+    </section>
+  </main>;  
 }
