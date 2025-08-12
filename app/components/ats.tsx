2a7f1f02cce46f6f87a4a9aac0bd404f70e22cd6
Zulqarnain <zulqarnain.maqsood42@gmail.comm>
2025-08-12 17:57:37 +0400

    resume feedback page

---
+++ app/components/ats.tsx
@@ -0,0 +1,10 @@
+const ATS = () =>{
+    return (
+        <div className="flex flex-col gap-8 animate-in fade-in duration-1000">
+            <h3 className="text-2xl font-semibold">ATS Information</h3>
+            <p className="text-gray-700">Your resume has been analyzed by our ATS system.</p>
+        </div>
+    )
+}
+
+export default ATS;
